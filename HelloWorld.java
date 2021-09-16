4a64883f6c993d6968c417eeb97a77838ba18244
L1zav3ta <liza01700@gmail.com>
2021-09-16 10:00:08 +0300

    cherry  pick

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 public class Hello{
     public static void main(String[] args) {
+<<<<<<< HEAD
 	System.out.println("Hello world!1");
+=======
+	System.out.println("Hello world!\n");
+>>>>>>> a337a78 (add \n)
 	System.out.println("New Feature!");
     }
 }
